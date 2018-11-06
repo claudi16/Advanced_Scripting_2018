09ab6c652b816c71470ae6612de9fdf53a6ffb91
claudi <claudi1618@gmail.com>
2018-11-05 23:07:50 -0700

    Python renamer 80%

---
+++ Scripts/scripts/Python_Sequential_Renamer.py
@@ -0,0 +1,37 @@
+import maya.cmds as cmds
+
+def RenameSelected(renameInput):
+    sels = cmds.ls(sl=True)
+    
+    new = []
+    new = renameInput.split("#")
+    number = 1
+    
+    hashNums = len(renameInput) - (len(new[0]) + len(new[2]))
+    
+    for sel in sels:
+        paddedNumber = "0"
+        selPadCap = 0
+        
+        for i in range(number):
+            i+= 10
+            selPadCap +=1
+        
+        padSize = hashNums - selPadCap
+        
+        for i in range(padSize):
+            paddedNumber += "0"
+            
+        #paddedNumber.zfill(number)
+        paddedNumber += str(number)
+        number += 1
+    
+        rearrange = (new[0] + paddedNumber + new[2])
+        cmds.rename(sel, rearrange)
+        print(number)
+         
+    print (paddedNumber)
+   
+    
+RenameSelected ('hello_###_work')
+
